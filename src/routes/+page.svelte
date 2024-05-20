53d4b489da610454d1c1bf6943705df9cfee5f2f
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-05-20 03:23:00 +0200

    Fix some formatting issues by running prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,28 @@
-<div class="p-3 flex flex-col gap-3">
+<div class="flex flex-col gap-3 p-3">
 	<div
 		style="
 			background-image: url(/explore.jpg);
 			background-size: cover;
 			background-position: center center;
 		"
-		class="relative shadow rounded-lg px-2 py-24 overflow-hidden"
+		class="relative overflow-hidden rounded-lg px-2 py-24 shadow"
 	>
 		<a
 			href="/article"
-			class="absolute w-full h-full inset-0 backdrop-blur-md flex items-center  flex-col justify-between py-2"
+			class="absolute inset-0 flex h-full w-full flex-col items-center justify-between py-2 backdrop-blur-md"
 		>
 			<div />
-			<h3 class="text-white text-2xl font-bold">
-				Entdecken
-			</h3>
+			<h3 class="text-2xl font-bold text-white">Entdecken</h3>
 			<div>
-				<a class="text-blue-500" href="https://commons.wikimedia.org/wiki/File:Tokyo_night_banner.JPG">Bild Quelle</a>
+				<a
+					class="text-blue-500"
+					href="https://commons.wikimedia.org/wiki/File:Tokyo_night_banner.JPG">Bild Quelle</a
+				>
 			</div>
 		</a>
 	</div>
 
-	<a href="/bookmarked" class="outline outline-2 rounded-lg flex items-center justify-center py-8">
+	<a href="/bookmarked" class="flex items-center justify-center rounded-lg py-8 outline outline-2">
 		Gespeicherte Inhalte
 	</a>
 </div>
